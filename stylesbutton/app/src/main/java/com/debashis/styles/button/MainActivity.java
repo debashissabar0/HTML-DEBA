d5843935ddcfb507e9ed402daa92fe4b97a8c6f4
debashissabar0 <debashissabar0@gmail.com>
2023-04-27 14:21:47 +0530

    Debashis Sabar

---
+++ stylesbutton/app/src/main/java/com/debashis/styles/button/MainActivity.java
@@ -0,0 +1,34 @@
+package com.debashis.styles.button;
+ 
+import android.app.Activity;
+import android.os.Bundle;
+import com.debashis.styles.button.R;
+
+
+public class MainActivity extends Activity { 
+     
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+     /*   
+        StylesButton button = new StylesButton(this);
+        button.setText("Submit");
+        button.setTextColor(Color.RED);
+        button.setAllCaps(true);
+        button.setFontIcon("\uf138");
+        button.setIconPosition(NoboButton.POSITION_LEFT);
+        button.setBackgroundColor(Color.WHITE);
+        button.setFocusColor(Color.GRAY);
+        button.setBorderColor(Color.RED);
+        button.setBorderWidth(2);
+        button.setRadius(10); 
+        
+        */
+    }
+	
+} 
+/*This app was Remoded by nnamsy  Gmail  nnamspro@gmail.com*/
+/*
+Remeber To suscribe to my Youtube channel*/
